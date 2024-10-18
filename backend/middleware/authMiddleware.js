04234a897d63d75f6bf0ad5cb574072074d74e04
Igor Miquelin <igor.miquelin@outlook.com>
2024-10-18 17:03:17 -0300

    Ajustes de segurança

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,5 +1,6 @@
+require('dotenv').config(); // Para carregar as variáveis de ambiente
 const jwt = require('jsonwebtoken');
-const secretKey = 'mySecretKey';  // A mesma chave secreta usada no authController
+const secretKey = process.env.SECRET_KEY;  // A mesma chave secreta usada no authController
 
 const authenticateJWT = (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1];
